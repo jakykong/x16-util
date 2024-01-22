582ca2ad9f0db422b8137e7e40756b1fb966445c
Jack Mudge <jack@mudge.dev>
2024-01-21 21:05:29 -0800

    Add kernal includes to util.asm meta-include file

--- util.asm
+++ util.asm
@@ -1,7 +1,8 @@
-; Import all libraries in dependency order
+; Import ALL libraries in dependency order, and other useful imports.
 ; For normal library usage, include this file.
 ; For partial library usage, reference this for dependencies.
 
+.include "cbm_kernal.inc" ; Build requires cc65\asminc
 .include "intmath.asm"
 .include "io.asm" ; Depends: intmath.asm
 
